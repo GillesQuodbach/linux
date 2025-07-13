848281552c2deea0308de8877636ec23f219f91b
GillesQuodbach <quodbach.gilles@gmail.com>
2025-07-14 00:06:00 +0200

    fish modif

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    function vps
+	ssh -i ~/.ssh/id_rsa ubuntu@83.228.192.26
+    end
+    funcsave vps
 end
---
+++ fish/functions/vps.fish
@@ -0,0 +1,4 @@
+function vps
+	ssh -i ~/.ssh/id_rsa ubuntu@83.228.192.26
+    
+end
